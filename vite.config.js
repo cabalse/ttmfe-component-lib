133aa24f6f093a0d48c5951c727624f09a751886
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2025-04-14 17:05:40 +0200

    Adding Tests

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setup-tests.js"],
+  },
   build: {
     lib: {
       entry: "./src/index.js",
