a31b24239261ebc4ea3793511413bd6ad3cf7104
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2025-04-14 10:49:51 +0200

    Init lib

---
+++ src/components/button/button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick} className="btn">
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/index.js",
+      name: "MyReactCompLib",
+      fileName: (format) => `ttmfe-component-lib.${format}.js`,
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
